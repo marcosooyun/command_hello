ca94aa0e021528308d4e1624c0b2516270791201
marcosooyun <49188221+marcosooyun@users.noreply.github.com>
2019-04-02 21:48:00 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 //For command line git tutorial
 //First: Local repository modification
 //Second: GitHub modification
+
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
